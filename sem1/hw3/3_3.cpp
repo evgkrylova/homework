0fffad709c16c0728de34e21a60fd110ae9e6291
Evgeniya Krylova <evg.krylova1753@gmail.com>
2018-10-05 18:07:45 +0300

    saved 3_3

--- sem1/hw3/3_3.cpp
+++ sem1/hw3/3_3.cpp
@@ -1,67 +1,18 @@
-#include "stdafx.h"
-
 #include <iostream>
 
 using namespace std;
 
-int length(char *s)
+int exponentiating(int x, int n)
 {
-	int counter = 0;
-
-	while (s[counter] != '\0')
+	if (n == 0)
 	{
-		counter++;
+		return 1;
 	}
-
-	return counter;
-}
-
-bool isIn(char x, char *y)
-{
-	for (int i = 0; i < length(y); i++)
+	if (n % 2)
 	{
-		if (x == y[i])
-		{
-			return true;
-		}
+		return x * exponentiating(x, n - 1);
 	}
-	return false;
-}
-
-void bullsAndCows(char *wished, char *guessed)
-{
-	int bulls = 0;
-	int cows = 0;
-
-	for (int i = 0; i < length(guessed); i++)
-	{
-		if (guessed[i] == wished[i])
-		{
-			bulls++;
-		}
-		else if (isIn(guessed[i], wished))
-		{
-			cows++;
-		}
-	}
-
-	if (bulls == length(guessed))
-	{
-		cout << "You won! It took you " << tries << " tries.";
-	}
-	else
-	{
-		cout << "Answer: " << bulls << " bulls and " << cows << " cows.";
-	}
-
-}
-
-void sumView(int n)
-{
-	if (n == 1)
-	{
-		char* a = new char['1'];
-	}
+	return exponentiating(x, n / 2) * exponentiating(x, n / 2);
 }
 
 int mass(int n)
@@ -78,13 +29,15 @@ int mass(int n)
 
 int main()
 {
-	int* a = new int[3];
-	a[0] = 4;
-	a[1] = 5;
-	a[2] = 29;
+	int number = 1;
+	cout << "Enter the base (b): ";
+	cin >> number;
+
+	int exponent = 0;
+	cout << "Enter the exponent (n): ";
+	cin >> exponent;
+
+	cout << "The result of exponentiation (b^n): " << exponentiating(number, exponent);
+
 	return 0;
 }
-
-
-
-
