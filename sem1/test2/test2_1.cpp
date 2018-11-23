bcb255fbb477365f1944dad808bb4e5741ea8871
Evgeniya Krylova <evg.krylova1753@gmail.com>
2018-11-23 15:43:24 +0300

    added task test2_1

---
+++ sem1/test2/test2_1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	int previousNumber = 0;
+	int currentNumber = 1;
+	for (int i = 1; i < n; i++)
+	{
+		currentNumber += previousNumber;
+		previousNumber = currentNumber - previousNumber;
+	}
+
+	return currentNumber;
+}
+
+int main()
+{
+	int number;
+	cout << "Enter the number of the Fibonacci sequence element: ";
+	cin >> number;
+	cout << "Result: " << fibonacci(number) << endl;
+
+    return 0;
+}
+
