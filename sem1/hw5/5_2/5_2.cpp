5a3d1725f692299420e3001163a31b8ffaa8cc73
Evgeniya Krylova <evg.krylova1753@gmail.com>
2018-11-25 19:45:22 +0300

    убрана повторяющаяся часть кода, изменены названия некоторых переменных

--- sem1/hw5/5_2/5_2.cpp
+++ sem1/hw5/5_2/5_2.cpp
@@ -3,12 +3,32 @@
 
 using namespace std;
 
+bool isLowercaseLetter(char symbol)
+{
+	int code = (int)(symbol);
+	return (code >= (int)('a')) & (code <= (int)('z'));
+}
+
+bool isCapitalLetter(char symbol)
+{
+	int code = (int)(symbol);
+	return (code >= (int)('A')) & (code <= (int)('Z'));
+}
+
+void cleanArray(bool *array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = false;
+	}
+}
+
 int main()
 {
 	ifstream fin;
 	fin.open("input.txt");
 
-	bool spaceWasDone = false;
+	bool isWordRead = false;
 	int const lettersAmount = 26;
 	bool *letters = new bool[lettersAmount];
 
@@ -16,35 +36,25 @@ int main()
 	{
 		letters[i] = false;
 	}
+	char symbol = '\0';
 
 	while (!fin.eof())
 	{
-		char symbol = '_';
 		fin.get(symbol);
-		int code = (int)(symbol);
+		int symbolNumber = (int)(symbol) - ((int)('a') * isLowercaseLetter(symbol) + (int)('A') * isCapitalLetter(symbol));
 
-		if ((code >= (int)('A')) & (code <= (int)('Z')))
+		if ((isCapitalLetter(symbol)) || ((isLowercaseLetter(symbol)) & (!letters[symbolNumber])))
 		{
-			letters[code - (int)('A')] = true;
+			letters[symbolNumber] = true;
 			cout << symbol;
-			spaceWasDone = false;
+			isWordRead = false;
 		}
 
-		if (((code >= (int)('a')) & (code <= (int)('z'))) & (!letters[code - (int)('a')]))
+		if (((isspace(symbol)) || (!isprint(symbol)) || (symbol == '-') || (symbol == '’')) & (!isWordRead))
 		{
-			letters[code - (int)('a')] = true;
-			cout << symbol;
-			spaceWasDone = false;
-		}
-
-		if (((symbol == ' ') || (symbol == '\n') || (symbol == '\t') || (symbol == '-')) & (!spaceWasDone))
-		{
-			for (int i = 0; i < lettersAmount; i++)
-			{
-				letters[i] = false;
-			}
+			cleanArray(letters, lettersAmount);
 			cout << endl;
-			spaceWasDone = true;
+			isWordRead = true;
 		}
 	}
 
@@ -53,3 +63,4 @@ int main()
 
 	return 0;
 }
+
