68a69beea91a9ec1ff51606176c272d0bd2cbd7c
Evgeniya Krylova <evg.krylova1753@gmail.com>
2018-12-10 02:17:34 +0300

    добавлена задача 8_3

---
+++ sem1/hw8/8_3/8_3.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include "HashTable.h"
+#include "HWstring.h"
+
+using namespace std;
+
+const int maxWordLength = 256;
+
+bool isLetter(char c)
+{
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+	{
+		return true;
+	}
+	return false;
+}
+
+String *readWord(ifstream &fin)
+{
+	char *word = new char[maxWordLength];
+	int index = 0;
+	char currentSymbol = '\0';
+
+	while ((!fin.eof()) && (isLetter(fin.peek()) || index == 0))
+	{
+		fin.get(currentSymbol);
+		if (isLetter(currentSymbol))
+		{
+			word[index] = currentSymbol;
+			index++;
+		}
+	}
+
+	return new String{ index, word };
+}
+
+int main()
+{
+	ifstream fin;
+	fin.open("input.txt");
+
+	HashTable *table = createHashTable();
+	String *word = createString();
+
+	while (!fin.eof())
+	{
+		word = readWord(fin);
+		addWord(table, word);
+	}
+
+	int choice = -1;
+	cout << "Enter the command if you want:\n\t 0 - to exit;\n";
+	cout << "\t 1 - to print 'load factor';\n";
+	cout << "\t 2 - to print the maximum of words' numbers of attempts while adding;\n";
+	cout << "\t 3 - to print these words;\n";
+	cout << "\t 4 - to print the average of these values;\n";
+	cout << "\t 5 - to print how many different words file includes;\n";
+	cout << "\t 6 - to print the number of empty buckets cells in the Hash Table\n";
+	cout << "\nYour command: ";
+	cin >> choice;
+
+	while (choice != 0)
+	{
+		switch (choice)
+		{
+		case 1:
+			cout << "Load factor: " << loadFactor(table);
+			break;
+		case 2:
+			cout << "Maximum of attempts: " << maximumAttemptsNumber(table);
+			break;
+		case 3:
+			cout << "Words with maximum of attempts: ";
+			printWordsWithMaximumAttemptsNumber(table);
+			break;
+		case 4:
+			cout << "Average of attempts: " << averageAttemptsNumber(table);
+			break;
+		case 5:
+			cout << "Number of words: " << wordsNumber(table);
+			break;
+		case 6:
+			cout << "Number of empty buckets' cells: " << emptyBucketsCellsNumber(table);
+			break;
+		}
+
+		cout << "\nThe process was successfully completed.\n\nYour next command: ";
+		cin >> choice;
+	}
+
+	deleteString(word);
+	deleteHashTable(table);
+
+	return 0;
+}
+
---
+++ sem1/hw8/8_3/HWstring.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include "HWstring.h"
+
+using namespace std;
+
+String *createString()
+{
+	return new String{ 0, nullptr };
+}
+
+void deleteString(String *&string)
+{
+	delete[] string->symbols;
+	delete string;
+}
+
+void readString(String *string)
+{
+	const int maxLength = 256;
+
+	int length = 0;
+	char *readingString = new char[maxLength];
+
+	char currentSymbol = '\0';
+	cin.ignore(cin.rdbuf()->in_avail());
+	cin.get(currentSymbol);
+
+	int index = 0;
+	while (currentSymbol != '\n')
+	{
+		readingString[index] = currentSymbol;
+		index++;
+		cin.get(currentSymbol);
+	}
+	length = index;
+
+	string->length = length;
+	string->symbols = new char[length];
+
+	for (int i = 0; i < length; i++)
+	{
+		string->symbols[i] = readingString[i];
+	}
+}
+
+String *clone(String *string)
+{
+	return getSubstring(string, 0, string->length - 1);
+}
+
+String *concatenate(String *firstString, String *secondString)
+{
+	String *sumString = createString();
+
+	int sumStringLength = firstString->length + secondString->length;
+	sumString->length = sumStringLength;
+	sumString->symbols = new char[sumStringLength];
+
+	for (int i = 0; i < firstString->length; i++)
+	{
+		sumString->symbols[i] = firstString->symbols[i];
+	}
+	for (int i = firstString->length; i < sumStringLength; i++)
+	{
+		sumString->symbols[i] = secondString->symbols[i - firstString->length];
+	}
+
+	return sumString;
+}
+
+bool areEqual(String *firstString, String *secondString)
+{
+	if (firstString->length != secondString->length)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < firstString->length; i++)
+	{
+		if (firstString->symbols[i] != secondString->symbols[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int length(String *string)
+{
+	return string->length;
+}
+
+bool isEmpty(String *string)
+{
+	return (string->length == 0);
+}
+
+String *getSubstring(String *string, int begin, int end)
+{
+	String *substring = createString();
+
+	int substringLength = end - begin + 1;
+	substring->length = substringLength;
+	substring->symbols = new char[substringLength];
+
+	for (int i = 0; i < substringLength; i++)
+	{
+		substring->symbols[i] = string->symbols[begin + i];
+	}
+
+	return substring;
+}
+
+char *convertToChar(String *string)
+{
+	return string->symbols;
+}
+
+void printString(String *string)
+{
+	char *array = convertToChar(string);
+	int size = length(string);
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i];
+	}
+}
