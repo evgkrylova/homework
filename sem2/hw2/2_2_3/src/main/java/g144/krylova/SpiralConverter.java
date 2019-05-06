e8fb2ddb216c575f86f2e9504bf61855e0faaf4e
Evgeniya Krylova <evg.krylova1753@gmail.com>
2019-05-07 02:47:22 +0300

    matrix is printed right away

--- sem2/hw2/2_2_3/src/main/java/g144/krylova/SpiralConverter.java
+++ sem2/hw2/2_2_3/src/main/java/g144/krylova/SpiralConverter.java
@@ -11,27 +11,13 @@ abstract class SpiralConverter implements SpiralPrinter{
      * {@inheritDoc}
      */
     void printSpiral(int[][] matrix, PrintStream output) throws IncorrectSizeException{
-        String resultString = spiralForm(matrix);
-        output.print(resultString);
-    }
-
-    /**
-     * Method converting matrix to the string of the spiral form.
-     * @param matrix is the array to convert.
-     * @return spiral string.
-     * @throws IncorrectSizeException if it is impossible to read the matrix array as spiral.
-     */
-    static public String spiralForm(int[][] matrix) throws IncorrectSizeException{
-        StringBuffer resultString = new StringBuffer();
-
         if (matrix.length % 2 == 0 || matrix.length < 1) {
             throw new IncorrectSizeException("Incorrect size of matrix.");
         }
 
         int horizontalIndex = matrix.length / 2;
         int verticalIndex = matrix.length / 2;
-        resultString.append(matrix[horizontalIndex][verticalIndex]);
-        resultString.append(" ");
+        output.print(matrix[horizontalIndex][verticalIndex] + " ");
 
         for (int i = 1; i < matrix.length; i++) {
             for (int k = 0; k < i * 2; k++) {
@@ -41,26 +27,20 @@ abstract class SpiralConverter implements SpiralPrinter{
                     } else {
                         horizontalIndex--;
                     }
-                }
-                else{
+                } else {
                     if (k < i) {
                         verticalIndex--;
                     } else {
                         horizontalIndex++;
                     }
                 }
-
-                resultString.append(matrix[horizontalIndex][verticalIndex]);
-                resultString.append(" ");
+                output.print(matrix[horizontalIndex][verticalIndex] + " ");
             }
         }
 
         for (int k = 0; k < matrix.length - 1; k++) {
             verticalIndex++;
-            resultString.append(matrix[horizontalIndex][verticalIndex]);
-            resultString.append(" ");
+            output.print(matrix[horizontalIndex][verticalIndex] + " ");
         }
-
-        return resultString.toString();
     }
 }
--- sem2/hw2/2_2_3/src/test/java/g144/krylova/SpiralConverterTest.java
+++ sem2/hw2/2_2_3/src/test/java/g144/krylova/SpiralConverterTest.java
@@ -4,19 +4,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 class SpiralConverterTest {
 
     @Test
-    void spiralFormTest() throws IncorrectSizeException{
-        int[][] input = {{5, 4, 3}, {6, 1, 2}, {7, 8, 9}};
+    void spiralFormTest() throws IOException, IncorrectSizeException{
+        ByteArrayOutputStream testStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testStream));
+        int[][] inputArray = {{5, 4, 3}, {6, 1, 2}, {7, 8, 9}};
         String correctAnswer = "1 2 3 4 5 6 7 8 9 ";
-        String answer = SpiralConverter.spiralForm(input);
-        assertEquals(correctAnswer, answer);
+        SpiralPrinter output = new ConsolePrinter();
+        output.printSpiral(inputArray);
+        assertEquals(correctAnswer, testStream.toString());
     }
 
     @Test
     void IncorrectSizeTest(){
-        int[][] input = {{5, 4}, {6, 1}};
-        assertThrows(IncorrectSizeException.class, () -> SpiralConverter.spiralForm(input));
+        int[][] inputArray = {{5, 4}, {6, 1}};
+        SpiralPrinter output = new ConsolePrinter();
+        assertThrows(IncorrectSizeException.class, () -> output.printSpiral(inputArray));
     }
 }
