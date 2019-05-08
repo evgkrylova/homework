188823bc0a0e70c3932c2f383ed0191d71f7bbe5
Evgeniya Krylova <evg.krylova1753@gmail.com>
2019-05-09 02:16:28 +0300

    added task 2_5_2

---
+++ sem2/hw5/2_5_2/src/main/java/g144/krylova/Calculator.java
@@ -0,0 +1,37 @@
+package g144.krylova;
+
+/**
+ * Class, contains method, which return result of operation with two values.
+ */
+public class Calculator {
+    /**
+     * Method calculating the result of basic operations.
+     * @param a is the first operand.
+     * @param b is the second operand.
+     * @param operation is operation to calculate.
+     * @return result of calculating.
+     * @throws ArithmeticException if the divider is zero.
+     */
+    public static double calculate(double a, double b, char operation) throws ArithmeticException{
+        switch (operation){
+            case '+':{
+                return a + b;
+            }
+            case '-':{
+                return a - b;
+            }
+            case '*':{
+                return a * b;
+            }
+            case '/':{
+                if (b == 0){
+                    throw new ArithmeticException();
+                }
+                return a / b;
+            }
+            default:{
+                return 0;
+            }
+        }
+    }
+}
