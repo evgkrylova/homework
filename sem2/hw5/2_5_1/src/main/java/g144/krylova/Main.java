f0a398fc73f0bb3faf9c4015bb5584da7bfef293
Evgeniya Krylova <evg.krylova1753@gmail.com>
2019-05-11 16:18:03 +0300

    zooming now

--- sem2/hw5/2_5_1/src/main/java/g144/krylova/Main.java
+++ sem2/hw5/2_5_1/src/main/java/g144/krylova/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sliderAndBar.fxml"));
         primaryStage.setTitle("Slider and progress bar");
         primaryStage.setScene(new Scene(root, 300, 100));
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 
